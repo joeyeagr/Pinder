49a289071c4950056cfacab925ca34f80f7d1991
Joe Eagar <joeeagar5588@gmail.com>
2019-03-26 09:25:35 -0600

    commit 2 installed podfile, started storyboard, etc.

--- Pinder/ViewController.swift
+++ Pinder/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var petPicture: UIImageView!
+    
+    func setCornerRounds() {
+        petPicture.layer.cornerRadius = 50.0
+        petPicture.layer.masksToBounds = true
+        petPicture.layer.shadowColor = UIColor(red: 0/255.0, green: 0/255.0, blue: 0/255.0, alpha: 1.0).cgColor
+        petPicture.layer.shadowOffset = CGSize(width: 0, height: 1.75)
+        petPicture.layer.shadowRadius = 1.7
+        petPicture.layer.shadowOpacity = 0.45
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setCornerRounds()
+
     }
-
-
+    
+    
 }
 
