f18048bf42234e325dd0dd02168ae4fe621e4af4
Tyler Donohue <donohue_tyler@yahoo.com>
2019-04-01 09:02:57 -0600

    link the tableviews and added ui, made outlets and buttons for the views.

---
+++ Pinder/Model/Pet.swift
@@ -0,0 +1,10 @@
+//
+//  Pet.swift
+//  Pinder
+//
+//  Created by Tyler Donohue on 3/29/19.
+//  Copyright © 2019 Joe Eagar. All rights reserved.
+//
+
+import Foundation
+// make sure to add a date for when the post was created.
