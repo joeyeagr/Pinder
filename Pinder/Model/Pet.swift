b409e709c71ddda2cb60bc9e56f22321823f1108
Tyler Donohue <donohue_tyler@yahoo.com>
2019-04-01 10:14:01 -0600

    added pet struct.

--- Pinder/Model/Pet.swift
+++ Pinder/Model/Pet.swift
@@ -8,3 +8,34 @@
 
 import Foundation
 // make sure to add a date for when the post was created.
+import FirebaseAuth
+import FirebaseStorage
+import FirebaseFirestore
+import Firebase
+
+struct Pet {
+    var petId: String
+    var petName: String
+    var petBreed: String
+    var petAge: Int
+    var petGender: Array = ["Male", "Female"]
+    var petBio: String
+    var petImage1: UIImage
+    var petImage2: UIImage
+    
+    var dictionary: [String: Any] {
+        return [
+            "id": petId,
+            "petName": petName,
+            "petBreed": petBreed,
+            "petAge": petAge,
+            "petGender": petGender,
+            "petBio": petBio,
+            "petImage1": petImage1,
+            "petImage2": petImage2
+            
+            
+        ]
+    }
+    
+}
