a8947a023cf6aa5dc4c0551eaff9ace2fce91515
benjamin7p <benjamin7p@gmail.com>
2019-04-09 10:42:51 -0600

    just some edge insets

--- Pinder/ViewControllers/PetCardCollectionViewController.swift
+++ Pinder/ViewControllers/PetCardCollectionViewController.swift
@@ -17,7 +17,9 @@ class PetCardCollectionViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+
+        self.collectionView.contentInset = sectionInsets
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
